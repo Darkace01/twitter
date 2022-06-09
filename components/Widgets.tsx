7298388d61053282b06abe140b09534215064b14
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-09 23:27:49 +0100

    Search Component

---
+++ components/Widgets.tsx
@@ -0,0 +1,18 @@
+import { SearchIcon } from '@heroicons/react/outline';
+
+function Widgets() {
+  return (
+    <div className='px-2 mt-2'>
+      <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2'>
+        <SearchIcon className='h-5 w-5 text-gray-400' />
+        <input
+          type='text'
+          placeholder='Search Twitter'
+          className='flex-1 bg-transparent active:outline-none outline-none'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Widgets;
