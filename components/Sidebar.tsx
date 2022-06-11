a83b3b4a97546be62fbac10a8eaa7002526bba1e
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-11 18:09:51 +0100

    Google authentication setup

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,9 +8,11 @@ import {
   UserIcon,
   HomeIcon,
 } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import SidebarRow from './SidebarRow';
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
       <img
@@ -24,7 +26,11 @@ function Sidebar() {
       <SidebarRow Icon={MailIcon} title='Messages' />
       <SidebarRow Icon={BookmarkIcon} title='Bookmarks' />
       <SidebarRow Icon={CollectionIcon} title='Lists' />
-      <SidebarRow Icon={UserIcon} title='Sign In' />
+      <SidebarRow
+        onClick={session ? signOut : signIn}
+        Icon={UserIcon}
+        title={session ? 'Sign Out' : 'Sign In'}
+      />
       <SidebarRow Icon={DotsCircleHorizontalIcon} title='More' />
     </div>
   );
