70803e8d42c39c771aea3a719fd41f93dd1ab04b
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-11 14:06:27 +0100

    comment section

--- pages/api/getComments.ts
+++ pages/api/getComments.ts
@@ -4,21 +4,22 @@ import { sanityClient } from '../../sanity'
 import { Comment } from '../../typings'
 import { groq } from 'next-sanity'
 
-const feedQuery = groq`
-    *[_type == "comment" ] {
+const commentQuery = groq`
+    *[_type == "comment" && references(*[_type == "tweet" && _id== $tweetId]._id)] {
     _id,
     ...
     } | order(_createdAt desc)
 `
 
-type Data = {
-    comments: Comment[]
-}
+type Data = Comment[]
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    const comments: Comment[] = await sanityClient.fetch(feedQuery);
-    res.status(200).json({ comments: comments })
+    const { tweetId } = req.query;
+    const comments: Comment[] = await sanityClient.fetch(commentQuery, {
+        tweetId,
+    });
+    res.status(200).json(comments)
 }
