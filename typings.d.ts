b46d62bbd149c024a9919dc54e55c95726a6b5aa
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-11 13:12:45 +0100

    Fetch comments

--- typings.d.ts
+++ typings.d.ts
@@ -11,3 +11,22 @@ export type TweetBody = {
     profileImg: string
     image?: string
 }
+
+export type CommentBody = {
+    comment: string
+    tweetId: string
+    username: string
+    profileImg: string
+}
+
+export interface Comment extends CommentBody {
+    _id: string;
+    _createdAt: string;
+    _updatedAt: string;
+    _type: 'tweet';
+    _rev: string;
+    tweet: {
+        _ref: string
+        _type: 'reference'
+    }
+}
