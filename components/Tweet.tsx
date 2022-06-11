54d4a68b51576d3150125ec4ef7d43cc2b4330cc
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-11 20:35:38 +0100

    Comments add and fetch

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import { Comment, Tweet } from '../typings';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Comment, CommentBody, Tweet } from '../typings';
 import TimeAgo from 'react-timeago';
 import {
   ChatAlt2Icon,
@@ -9,20 +9,60 @@ import {
   UploadIcon,
 } from '@heroicons/react/outline';
 import { fetchComments } from '../utils/fetchComments';
+import { useSession } from 'next-auth/react';
+import toast from 'react-hot-toast';
 
 interface Props {
   tweet: Tweet;
 }
 function Tweet({ tweet }: Props) {
   const [comments, setComments] = useState<Comment[]>([]);
+  const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false);
+  const [input, setInput] = useState<string>('');
+  const [isRefreshingComments, setIsRefreshingComments] =
+    useState<boolean>(false);
   const refreshComments = async () => {
     const comments: Comment[] = await fetchComments(tweet._id);
     setComments(comments);
   };
-
+  const { data: session } = useSession();
   useEffect(() => {
     refreshComments();
   }, []);
+
+  useEffect(() => {
+    refreshComments();
+    setIsRefreshingComments(false);
+  }, [isRefreshingComments]);
+
+  const postComment = async () => {
+    const commentBody: CommentBody = {
+      comment: input,
+      username: session?.user?.name || 'Unknown User',
+      profileImg: session?.user?.image || 'https://links.papareact.com/gll',
+      tweetId: tweet._id,
+    };
+    const result = await fetch('/api/addComment', {
+      body: JSON.stringify(commentBody),
+      method: 'POST',
+    });
+
+    const json = await result.json();
+
+    toast('Comment Posted', {
+      icon: '🕊️',
+    });
+    return json;
+  };
+
+  const handleComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    postComment();
+    setIsRefreshingComments(true);
+    setInput('');
+    setCommentBoxVisible(false);
+  };
+
   return (
     <div className='flex flex-col space-x-3 border-y p-5 border-gray-100'>
       <div className='flex space-x-3 '>
@@ -61,7 +101,10 @@ function Tweet({ tweet }: Props) {
         </div>
       </div>
       <div className='mt-5 flex justify-between'>
-        <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
+        <div
+          className='flex cursor-pointer items-center space-x-3 text-gray-400'
+          onClick={() => session && setCommentBoxVisible(!commentBoxVisible)}
+        >
           <ChatAlt2Icon className='h-5 w-5' />
           <p>{comments?.length}</p>
         </div>
@@ -76,8 +119,26 @@ function Tweet({ tweet }: Props) {
         </div>
       </div>
       {/* TODO: Comment box logic */}
+      {commentBoxVisible && (
+        <form className='mt-3 flex space-x-3' onSubmit={handleComment}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            placeholder='Write a comment...'
+            className='flex-1 rounded-lg bg-gray-100 p-2 outline-none'
+          />
+          <button
+            className='text-twitter disabled:text-gray-200'
+            disabled={!input}
+            type='submit'
+          >
+            Post
+          </button>
+        </form>
+      )}
       {comments?.length > 0 && (
-        <div className='my-2 mt-5 max-4-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
+        <div className='my-2 mt-5 max-4-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5 scrollbar-hide'>
           {comments?.map((comment) => (
             <div key={comment._id} className='relative flex space-x-2'>
               <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30' />
