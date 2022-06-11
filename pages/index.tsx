24e251166ceefa52fdf37770e30ab3dd38776d03
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-06-11 11:10:45 +0100

    Making a sanity fetch call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import Feed from '../components/Feed';
 import Sidebar from '../components/Sidebar';
 import Widgets from '../components/Widgets';
@@ -22,3 +21,10 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // const tweets = await
+  return {
+    props: {},
+  };
+};
